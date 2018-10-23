8b44dba909f1986f9b3f3e712c13d383a601d7eb
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-10-23 17:00:17 +0900

    Update index.php

--- index.php
+++ index.php
@@ -17,74 +17,17 @@
      
  
  
-  <form method="post" action="index.php">
-        名前　　　　<input type="text" name="name">
-        メッセージ　<input type="text" name="message">
- 
-        <button name="send" type="submit">送信</button>
- 
-        チャット履歴
-    <section>
-        <?php // DBからデータ(投稿内容)を取得 $stmt = select(); foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $message) {
-                // 投稿内容を表示
-                //require_once "Common.php";
-                /*
-                echo $message['time'],"：　",$message['name'],"：",$message['message'];
-                echo nl2br("\n");
-            }
- 
-            // 投稿内容を登録
-            if(isset($_POST["send"])) {
-                insert();
-                // 投稿した内容を表示
-                $stmt = select_new();
-                foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $message) {
-                    echo $message['time'],"：　",$message['name'],"：",$message['message'];
-                    echo nl2br("\n");
-                }
-            }
- 
-            // DB接続
-            function connectDB() {
-                //$dbh = new PDO('mysql:host=localhost;dbname=chat','root','');
-                //$dbh = new Common();
-                
-                $url = parse_url(getenv('DATABASE_URL'));
-	              $dsn = sprintf("pgsql:host=%s;dbname=%s",$url['host'],substr($url['path'],1));	
-	              $dbh = new PDO($dsn,$url['user'],$url['pass']);
-                
-                return $dbh;
-            }
- 
-            // DBから投稿内容を取得
-            function select() {
-                $dbh = connectDB();
-                $sql = "SELECT * FROM message ORDER BY time";
-                $stmt = $dbh->prepare($sql);
-                $stmt->execute();
-                return $stmt;
-            }
- 
-            // DBから投稿内容を取得(最新の1件)
-            function select_new() {
-                $dbh = connectDB();
-                $sql = "SELECT * FROM message ORDER BY time desc limit 1";
-                $stmt = $dbh->prepare($sql);
-                $stmt->execute();
-                return $stmt;
-            }
- 
-            // DBから投稿内容を登録
-            function insert() {
-                $dbh = connectDB();
-                $sql = "INSERT INTO message (name, message, time) VALUES (:name, :message, now())";
-                $stmt = $dbh->prepare($sql);
-                $params = array(':name'=>$_POST['name'], ':message'=>$_POST['message']);
-                $stmt->execute($params);
-            }*/
-        ?>
-    </section>
-  </form>
+  	<form method="post" action="config.php">
+		<input type="textarea" name="sql">
+		<br>
+		<button name="send" type="submit">送信</button>
+	</form>
+	
+        <h2>実行履歴<h2><br>
+<?php
+
+?>
+
  
  
  

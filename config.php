3047ba7123aecdfebf59c950ddcbf902ef0d06ea
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-10-25 17:31:14 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,15 @@
 <?php
-
-
-
-
-
-
-
-
-
+isset($_POST['send']){  //実行ボタンが押されたら
+  $sql = $_POST['sql'];   //入力されたSQL文
+  require_once "Common.php";
+  $dbm = new Common();    //インスタンス＆コンストラクタ実行　接続OK
+  $dbm->db_sql_only(sql);   //取得SQL実行
+  $dbm->db_sql("");
+  
+  
+  
+  
+}
 
 
 ?>

02a2edde5f96650659c4a39a2cc66e3d9eacf1a5
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-10-26 21:25:21 +0900

    Update db_execute.php

--- db_execute.php
+++ db_execute.php
@@ -9,11 +9,19 @@
   $dbm->db_sql_only(sql);   //取得SQL実行
   //$dbm->db_sql("");         //現在未実装、Log表示用のSQLを挿入
   
+  $file = fopen("db_logs.txt","w");     //書き込みモード
+  fwrite($file,"テストテキスト");
+  fclose($file);
   
+  fopen("db_logs.txt","r");             // 読み込みモード
+    while ($line = fgets($file)) {
+      echo "$line<br />";
+    }
+  fclose($fp);
   
   
 //}
 
-header("Location: ./index.php")  ;       //ページの遷移先指定
+//header("Location: ./index.php")  ;       //ページの遷移先指定
 
 ?>

281a7c5f166d8df7b3af0e392fb2a9b38ed5cb51
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-10-26 16:23:29 +0900

    Update db_execute.php

--- db_execute.php
+++ db_execute.php
@@ -1,8 +1,8 @@
 <?php
 /* 実行ボタンを押したときの動作PHPファイル　　※画面戻り先リンク要指定 */
 
-if(isset($_POST['sql_submit'])){  //実行ボタンが押されたら
-  $sql = $_POST['sql'];   //入力されたSQL文
+if(isset($_POST["sql_submit"])){  //実行ボタンが押されたら
+  $sql = $_POST["sql"];   //入力されたSQL文
   require "Common.php";
   $dbm = new Common();    //インスタンス＆コンストラクタ実行　接続OK
   

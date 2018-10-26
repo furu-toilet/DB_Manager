94ef8fbcfc32600f86b021105a6d9d9dc8773ffe
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-10-26 21:31:15 +0900

    Update db_execute.php

--- db_execute.php
+++ db_execute.php
@@ -6,7 +6,7 @@
   require "Common.php";
   $dbm = new Common();    //インスタンス＆コンストラクタ実行　接続OK
   
-  $dbm->db_sql_only(sql);   //取得SQL実行
+  $dbm->db_sql_only("tail -f $PGDATA/pg_log/postgresql-Mon.log");   //取得SQL実行
   //$dbm->db_sql("");         //現在未実装、Log表示用のSQLを挿入
   
   $file = fopen("db_logs.txt","w");     //書き込みモード

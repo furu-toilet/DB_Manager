61fda05c8774238491c60c7fa59ac91c996b6782
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-10-30 15:46:47 +0900

    Update table_all.php

--- table_all.php
+++ table_all.php
@@ -6,8 +6,17 @@ $sql = "select pg_statio_user_tables.relname
         where relkind='r'
         and pg_catalog.pg_statio_user_tables.relid=pg_catalog.pg_class.relfilenode;";   //テーブル一覧を取得（名前のみ）
 
+$db = new Common();
+$tbal = $bd->db_sql($sql);           //テーブル一覧を取得（名前のみ）
+
+foreach($tbal as $value){
+        echo $value;
+}
 
 
 
+
+
+$$db->db_close();
 
 ?>
